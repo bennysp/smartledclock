1a77b2ba2c4d6c232d52cb07c5a242a855c9bc05
unknown <benny@bensurface.domain.thedaily.tv>
2017-12-20 16:47:56 -0600

    Fixed startup scripts

--- smartclock_daemon.sh
+++ smartclock_daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-basedir=/home/pi/piclock/smartledclock
+basedir=/home/pi/smartclock
 
 sudo ${basedir}/clock --led-gpio-mapping=adafruit-hat --led-chain=2 --led-slowdown-gpio=3 -b 50 -f ${basedir}/fonts/7x14B.bdf -x 5 -y 9 -d '%I:%M %p' &
 
