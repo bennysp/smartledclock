8d6a69ff31ce078e5f64decbc223815e039ee0c6
unknown <benny@bensurface.domain.thedaily.tv>
2017-12-20 19:39:54 -0600

    Updated script with configuration that reduces flickering

--- clock_daemon.sh
+++ clock_daemon.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
+#====================
+# Configure Settings
+#====================
+
+# Set Base Directory 
+
 basedir=/home/pi/smartledclock
 
-sudo ${basedir}/clock --led-gpio-mapping=adafruit-hat --led-chain=2 --led-slowdown-gpio=3 -b 50 -f ${basedir}/fonts/7x14B.bdf -x 5 -y 9 -d '%I:%M %p' &
+
+# Set the brightness here
+
+#brightness=100
+brightness=50
+#brightness=25
+
+
+# Set you color here
+
+#color=255,0,0 #red
+#color=25,0,0 #dark red
+#color=0,255,0 #green
+#color=0,0,255 #blue
+color=255,255,255 #white
+
+
+#=================
+# Run the script
+#================= 
+
+# For raspberry pi 2/3, you need to set the slowndown for gpio; 
+# mapping the adafruit hat for gpio
+# Reduce the PWM bits - at the default of 11, when you reduce the brightness, it flickers, this prevents flickering
+sudo ${basedir}/clock --led-gpio-mapping=adafruit-hat --led-chain=2 --led-slowdown-gpio=2 -b ${brightness} -f ${basedir}/fonts/7x14B.bdf -x 5 -y 0.5 -C ${color} --led-brightness=${brightness} --led-pwm-bits=5 -d '%I:%M %p' &
 
 exit 0
